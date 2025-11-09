e70f5c2dfe3759978dbc1e38c02f4dd0c7ac28aa
Rafael <60099368+HttpRafa@users.noreply.github.com>
2025-11-09 15:36:03 +0100

    Revert "Fixed hash logic on macOS (#82)"
    This reverts commit 62e32956f5934370d86fc2ffbf3ec1edd5a5b821.

--- common/src/main/java/de/rafael/modflared/binary/download/DownloadedCloudflared.java
+++ common/src/main/java/de/rafael/modflared/binary/download/DownloadedCloudflared.java
@@ -117,21 +117,22 @@ public class DownloadedCloudflared extends Cloudflared {
                 for (int i = 0; i < 4; i++) {
                     Modflared.LOGGER.info("Downloading cloudflared version {} from github. Attempt: {}", version, i + 1);
                     var downloadedFile = syncDownloadFile();
+                    Modflared.LOGGER.info("Downloaded file preparing cloudflared binary...");
                     var file = new File(TunnelManager.DATA_FOLDER, download.fileName());
+                    prepareFile(downloadedFile, file);
+
                     // Check if file is corrupt
                     Modflared.LOGGER.info("Checking file integrity");
-                    var provided = GithubAPI.FileHash.computeHash(downloadedFile);
+                    var provided = GithubAPI.FileHash.computeHash(file);
                     if(expected.compareTo(provided)) {
                         Modflared.LOGGER.info("Download finished of cloudflared version {}!", version);
-                        Modflared.LOGGER.info("Downloaded file preparing cloudflared binary...");
-                        prepareFile(downloadedFile, file);
                         return;
                     } else {
                         Modflared.LOGGER.warn("This downloaded file does not match with the file hash provided on GitHub.");
                         Modflared.LOGGER.warn("Expected {}, Provided: {}", expected.hash(), provided.hash());
-                        downloadedFile.delete();
-                    }
 
+                        file.delete();
+                    }
                 }
             } catch (InterruptedException exception) {
                 throw new IllegalStateException("Error while unpacking MacOS cloudflared download", exception);
